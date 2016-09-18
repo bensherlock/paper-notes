23adf10d0f55366f4d8319690180f68341d32049
Ben Sherlock <github@bensherlock.co.uk>
2016-09-18 21:59:22 +0100

    Working on using LokiJS/DiskDB with Backbone. Have something sort of working as a demonstrator, but lots of commented out debug-mess now needs cleaning up. :)

--- app/assets/js/collections/papers.js
+++ app/assets/js/collections/papers.js
@@ -1,9 +1,13 @@
+"use strict";
+// Backbone Collection: Papers
+
 var Backbone = require('backbone')
 
 // Local Storage - Load the data in from a user file
 require('backbone.localstorage')
 
 var $ = window.$;
+
 
 //Backbone.Collection.prototype.save = function (options) {
 //  Backbone.sync("create", this, options);
@@ -12,50 +16,20 @@ var $ = window.$;
 var PaperList = Backbone.Collection.extend({
   model: Paper,
 
-  localStorage: new Backbone.LocalStorage("papers-backbone"),
+  //localStorage: new Backbone.LocalStorage("papers-backbone"),
   //localStorage: true,
   //url: 'http://rest.com/api/',
   //localStorage: new Store('papers-backbone'),
-  //url: '/papers',
+  url: '/papers',
+
+
 
   nextOrder: function() {
     if(!this.length) return 1;
     return this.last().get('order') + 1;
   },
 
-/*
-  save: function( options ) {
-    var self = this;
-
-    var success = options.success;
-    var error = options.error;
-    var complete = options.complete;
-
-    options.success = function( response, status, xhr ) {
-      self.trigger('sync', self, response, options);
-      if (success) return success.apply(this, arguments);
-    };
-
-    options.error = function( response, status, xhr ) {
-      self.trigger('error', self, response, options);
-      if (error) return error.apply(this, arguments);
-    };
-
-    options.complete = function( response, status, xhr ) {
-      if (complete) return complete.apply(this, arguments);
-    }
-
-    Backbone.sync('create', this, options);
-  },
-  */
-
   comparator: 'order'
 });
 
-
 var Papers = new PaperList;
-
-// Force the collection to save to server when changes are made locally.
-//Papers.bind('change add remove reset', function(){
-//    this.save({});
-//});
