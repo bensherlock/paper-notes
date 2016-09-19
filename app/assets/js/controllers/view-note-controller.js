a5f39cedc33c0d548dad10996516bdd473d11770
Ben Sherlock <github@bensherlock.co.uk>
2016-09-19 22:58:50 +0100

    Working out how best to do the Notes - either as a nested Backbone Collection, or as separate Collection in the DB, or just as nested array+objects. But then need to work out how to load a specific instance for view/edit.

---
+++ app/assets/js/controllers/view-note-controller.js
@@ -0,0 +1,75 @@
+// Controller: ViewNoteController
+
+// Includes
+//var Backbone = require('backbone')
+var $ = window.$;
+
+// Container where the view will be rendered.
+var $container = $('#app-view-container');
+
+var editNoteController = function(paperId, noteId) {
+  bothNoteController(paperId, noteId, true);
+}
+
+var viewNoteController = function(paperId, noteId) {
+  bothNoteController(paperId, noteId, false);
+}
+
+var bothNoteController = function(paperId, noteId, editing) {
+  console.log('bothNoteController: paperId=' + paperId + " noteId=" + noteId);
+  // Check a collection to see if paper with id `id` exists,
+  // and if not instance a new one and fetch it.
+  var paper = Papers.get(paperId);
+
+  if( paper ) {
+    console.log('paper is already in collection');
+
+    // Get Note
+    var note = paper.notes.get(noteId);
+
+    if(note) {
+      var view;
+      if( editing ) {
+        view = new NoteEditView({model: note});
+      } else {
+        view = new NoteView({model: note});
+      }
+
+      $container.empty().append( view.render().el );
+    } else {
+      // Note isn't in the collection. Fetch it. How?
+      note = new Note({})
+
+    }
+
+
+
+
+  } else {
+    // Paper isn't in the collection. Fetch it.
+    paper = new Paper({id: id});
+    paper.fetch({
+      success : function() {
+        console.log('success fetching paper');
+        Papers.add(paper);
+
+        var view;
+        if( editing ) {
+          view = new PaperEditView({model: paper});
+        } else {
+          view = new PaperView({model: paper});
+        }
+
+        $container.empty().append( view.render().el );
+      },
+      error : function(e) {
+        //console.log( 'keys:' + _.keys(err) );
+        console.log('error fetching paper: ' + e.id);
+      },
+      complete: function () {
+        //alert(' Service request completed ');
+      }
+    });
+  }
+
+}
